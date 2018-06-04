5bfbf0ac03605e5d63d3338a6914eee48bb3718b
Dollon <wwb199055@126.com>
2018-06-04 14:25:33 +0800

    修改部分代码

--- common/src/main/java/com/fanneng/common/image/glide/utils/GlideUtils.java
+++ common/src/main/java/com/fanneng/common/image/glide/utils/GlideUtils.java
@@ -3,7 +3,11 @@ package com.fanneng.common.image.glide.utils;
 import android.annotation.SuppressLint;
 import android.widget.ImageView;
 
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.fanneng.common.image.glide.config.GlideApp;
+import com.fanneng.common.image.glide.config.GlideCircleTransform;
+import com.fanneng.common.image.glide.config.RoundedRectanglesTransformation;
 import com.fanneng.common.utils.AppContextUtils;
 
 /**
@@ -37,6 +41,7 @@ public class GlideUtils {
         .load(url)
         .placeholder(errorImg)
         .error(errorImg)
+        .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
         .dontAnimate()
         .centerCrop()
         .into(imgView);
@@ -46,96 +51,90 @@ public class GlideUtils {
    * @param errorImg 错误和占位符图片
    * @param path     图片地址(drawable、mipmap下的图片)
    * @param imgView  需要显示的图片
-   *//*
+   */
   @SuppressLint("CheckResult")
   public static void show(int errorImg, int path, ImageView imgView) {
 
-    RequestOptions options = new RequestOptions();
-    options.placeholder(errorImg)
-        .error(errorImg)
-        .centerCrop()
-        .skipMemoryCache(true);
 
-    Glide.with(AppContextUtils.getActivity())
+    GlideApp.with(AppContextUtils.getActivity())
         .load(path)
-        .apply(options)
+        .placeholder(errorImg)
+        .error(errorImg)
+        .skipMemoryCache(true)
+        .dontAnimate()
+        .centerCrop()
         .into(imgView);
+
   }
 
 
-  *//**
+  /**
    * 圆角
    *
    * @param errorImg 错误的资源图片
    * @param url      图片链接
    * @param imgView  组件
-   *//*
+   */
   @SuppressLint("CheckResult")
   public static void showCircle(int errorImg, String url, ImageView imgView) {
 
-    RequestOptions options = new RequestOptions();
-    options.placeholder(errorImg)
+
+    GlideApp.with(AppContextUtils.getActivity())
+        .load(url)
+        .placeholder(errorImg)
         .error(errorImg)
-        .transform(new GlideCircleTransform())
-        .centerCrop()
-        .diskCacheStrategy(DiskCacheStrategy.RESOURCE);
-
-
-    Glide.with(AppContextUtils.getActivity()).load(url)
-        .apply(options)
+        .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
         .transition(DrawableTransitionOptions.withCrossFade())
+        .dontAnimate()
+        .centerCrop()
         .into(imgView);
-
 
   }
 
-  *//**
+  /**
    * 圆角
    *
    * @param errorImg 错误的资源图片
    * @param path     图片地址
    * @param imgView  组件
-   *//*
+   */
   @SuppressLint("CheckResult")
   public static void showCircle(int errorImg, int path, ImageView imgView) {
 
-    RequestOptions options = new RequestOptions();
-    options.placeholder(errorImg)
+
+    GlideApp.with(AppContextUtils.getActivity())
+        .load(path)
+        .placeholder(errorImg)
         .error(errorImg)
         .transform(new GlideCircleTransform())
-        .centerCrop();
-
-
-    Glide.with(AppContextUtils.getActivity()).load(path)
-        .apply(options)
+        .dontAnimate()
+        .centerCrop()
         .into(imgView);
 
 
   }
 
 
-  *//**
+  /**
    * 矩形圆角
    *
    * @param errorImg 错误的资源图片
    * @param url      图片地址
    * @param imgView  组件
-   *//*
+   */
   @SuppressLint("CheckResult")
   public static void showImageViewToRoundedCorners(int errorImg, String url, ImageView imgView) {
 
-
-    RequestOptions options = new RequestOptions();
-    options.placeholder(errorImg)
+    GlideApp.with(AppContextUtils.getActivity())
+        .load(url)
+        .placeholder(errorImg)
         .error(errorImg)
         .transform(new RoundedRectanglesTransformation(35, 0, RoundedRectanglesTransformation.CornerType.ALL))
-        .centerCrop()
-        .diskCacheStrategy(DiskCacheStrategy.RESOURCE);
-
-    Glide.with(AppContextUtils.getActivity()).load(url)
-        .apply(options)
         .transition(DrawableTransitionOptions.withCrossFade())
+        .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+        .dontAnimate()
+        .centerCrop()
         .into(imgView);
-  }*/
+  }
 
 }

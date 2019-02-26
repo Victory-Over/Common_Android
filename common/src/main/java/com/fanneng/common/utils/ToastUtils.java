0e7f4bed4d6f8aa7f6a34b1b57a1d473104df45e
YaYa170619 <luyufengc@enn.cn>
2019-02-26 20:38:16 +0800

    修复Toast BUG

--- common/src/main/java/com/fanneng/common/utils/ToastUtils.java
+++ common/src/main/java/com/fanneng/common/utils/ToastUtils.java
@@ -2,6 +2,8 @@ package com.fanneng.common.utils;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
 import android.widget.Toast;
 
 import io.reactivex.annotations.NonNull;
@@ -14,71 +16,76 @@ import io.reactivex.annotations.NonNull;
  */
 public class ToastUtils {
 
-  @SuppressLint("StaticFieldLeak")
-  private static Context context = AppContextUtils.getContext();
+    @SuppressLint("StaticFieldLeak")
+    private static Context context = AppContextUtils.getContext();
 
-  private static Toast toast;
+    private static Toast toast;
 
-  /**
-   * Toast内容，支持String、资源ID（int）、CharSequence
-   *
-   * @param msg String、CharSequence
-   */
-  public static void show(@NonNull String msg) {
-    showView(msg, Toast.LENGTH_SHORT);
-  }
-
-  /**
-   * 对toast的封装。线程安全，可以在非UI线程调用。
-   * Toast内容，支持String、资源ID（int）、CharSequence
-   *
-   * @param resId int、CharSequence
-   */
-  public static void show(int resId) {
-    showView(context.getString(resId), Toast.LENGTH_SHORT);
-  }
-
-  /**
-   * 对toast的简易封装。线程安全，可以在非UI线程调用。
-   * Toast内容，支持String、资源ID（int）、CharSequence
-   *
-   * @param msg      String、int、CharSequence
-   * @param duration Toast时间。eg:Toast.LENGTH_SHORT
-   */
-  public static void show(@NonNull String msg, int duration) {
-    showView(msg, duration);
-  }
-
-  /**
-   * 对toast的简易封装。线程安全，可以在非UI线程调用。
-   * Toast内容，支持String、资源ID（int）、CharSequence
-   *
-   * @param resId    String、int、CharSequence
-   * @param duration Toast时间。eg:Toast.LENGTH_SHORT
-   */
-  public static void show(int resId, int duration) {
-    showView(context.getString(resId), duration);
-  }
-
-
-  @SuppressLint("ShowToast")
-  private static void showView(CharSequence text, int duration) {
-    if (toast == null) {
-      toast = Toast.makeText(context, text, duration);
-    } else {
-      toast.setText(text);
+    /**
+     * Toast内容，支持String、资源ID（int）、CharSequence
+     *
+     * @param msg String、CharSequence
+     */
+    public static void show(@NonNull String msg) {
+        showView(msg, Toast.LENGTH_SHORT);
     }
-    toast.show();
-  }
 
-
-  /**
-   * 取消Toast
-   */
-  public static void cancel() {
-    if (toast != null) {
-      toast.cancel();
+    /**
+     * 对toast的封装。线程安全，可以在非UI线程调用。
+     * Toast内容，支持String、资源ID（int）、CharSequence
+     *
+     * @param resId int、CharSequence
+     */
+    public static void show(int resId) {
+        showView(context.getString(resId), Toast.LENGTH_SHORT);
     }
-  }
+
+    /**
+     * 对toast的简易封装。线程安全，可以在非UI线程调用。
+     * Toast内容，支持String、资源ID（int）、CharSequence
+     *
+     * @param msg      String、int、CharSequence
+     * @param duration Toast时间。eg:Toast.LENGTH_SHORT
+     */
+    public static void show(@NonNull String msg, int duration) {
+        showView(msg, duration);
+    }
+
+    /**
+     * 对toast的简易封装。线程安全，可以在非UI线程调用。
+     * Toast内容，支持String、资源ID（int）、CharSequence
+     *
+     * @param resId    String、int、CharSequence
+     * @param duration Toast时间。eg:Toast.LENGTH_SHORT
+     */
+    public static void show(int resId, int duration) {
+        showView(context.getString(resId), duration);
+    }
+
+
+    @SuppressLint("ShowToast")
+    private static void showView(CharSequence text, int duration) {
+
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(() -> {
+            if (toast == null) {
+                toast = Toast.makeText(context, text, duration);
+            } else {
+                toast.setText(text);
+            }
+            toast.show();
+        });
+
+    }
+
+
+    /**
+     * 取消Toast
+     */
+    public static void cancel() {
+        if (toast != null) {
+            toast.cancel();
+        }
+    }
 
 }
